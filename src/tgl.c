0233d6b59f7f77835d35a263dbaca9901584115e
Jason Lingle <jasonlingle@yahoo.com>
2012-06-02 13:57:41 -0600

    Add initial files.

---
+++ src/tgl.c
@@ -0,0 +1,12 @@
+/* Main program for TGL (Text Generation Language). */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+int main(int argc, char** argv) {
+  printf("hello world\n");
+  return 0;
+}
