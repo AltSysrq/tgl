4eb6cc4cc74e60a8d84434485b44b3d4edc9afa7
Jason Lingle <jasonlingle@yahoo.com>
2012-06-02 18:53:00 -0600

    Add OOM handling and add string appending functions.

--- src/tgl.c
+++ src/tgl.c
@@ -9,6 +9,26 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 
+#define EXIT_OUT_OF_MEMORY 255
+
+/* Versions of malloc and realloc that abort on memory exhaustion. */
+static inline void* tmalloc(size_t size) {
+  void* result = malloc(size);
+  if (!result) {
+    perror("malloc");
+    exit(EXIT_OUT_OF_MEMORY);
+  }
+  return result;
+}
+static inline void* trealloc(void* ptr, size_t size) {
+  void* result = realloc(ptr, size);
+  if (!result) {
+    perror("realloc");
+    exit(EXIT_OUT_OF_MEMORY);
+  }
+  return result;
+}
+
 /* BEGIN: String handling (strings may have NUL bytes within, and are not
  * NUL-terminated).
  */
@@ -30,7 +50,7 @@ static inline byte* string_data(string s) {
  */
 static string covert_string(char* str) {
   unsigned int len = strlen(str);
-  string result = malloc(sizeof(struct string) + len);
+  string result = tmalloc(sizeof(struct string) + len);
   result->len = len;
   memcpy(string_data(result), str, len);
   return result;
@@ -40,8 +60,36 @@ static string covert_string(char* str) {
  * The string must be free()d by the caller.
  */
 static string dupe_string(string str) {
-  string result = malloc(sizeof(struct string) + str->len);
+  string result = tmalloc(sizeof(struct string) + str->len);
   memcpy(result, str, sizeof(struct string) + str->len);
+  return result;
+}
+
+/* Creates and returns a new empty string. */
+static string empty_string() {
+  string result = tmalloc(sizeof(struct string));
+  result->len = 0;
+  return result;
+}
+
+/* Appends string a to string b, destroying a.
+ * Returns a string representing the concatenated strings.
+ */
+static string append_string(string a, string b) {
+  string result = trealloc(a, sizeof(struct string)+a->len+b->len);
+  memcpy(string_data(result) + result->len, string_data(b), b->len);
+  result->len += b->len;
+  return result;
+}
+
+/* Appends a cstring onto a, destroying a.
+ * Returns a string representing the concatenated strings.
+ */
+static string append_cstr(string a, const char* b) {
+  unsigned blen = strlen(b);
+  string result = trealloc(a, sizeof(struct string)+a->len+blen);
+  memcpy(string_data(result) + result->len, b, blen);
+  result->len += blen;
   return result;
 }
 /* END: String handling */
