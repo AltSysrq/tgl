608e78d43eb205fd6bb4755c5495a7d1460a159b
Jason Lingle <jasonlingle@yahoo.com>
2012-06-02 18:32:28 -0600

    Add length-prefixed string handling functions.

--- src/tgl.c
+++ src/tgl.c
@@ -1,10 +1,50 @@
 /* Main program for TGL (Text Generation Language). */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+
+/* BEGIN: String handling (strings may have NUL bytes within, and are not
+ * NUL-terminated).
+ */
+
+/* Defines a length-prefixed string.
+ * The string contents start at the byte after the struct itself.
+ */
+typedef struct string { unsigned len; }* string;
+typedef unsigned char byte;
+
+/* Returns a pointer to the beginning of character data of the given string. */
+static inline byte* string_data(string s) {
+  byte* c = (byte*)s;
+  return c + sizeof(struct string);
+}
+
+/* Converts a C string to a TGL string.
+ * The string must be free()d by the caller.
+ */
+static string covert_string(char* str) {
+  unsigned int len = strlen(str);
+  string result = malloc(sizeof(struct string) + len);
+  result->len = len;
+  memcpy(string_data(result), str, len);
+  return result;
+}
+
+/* Duplicates the given TGL string.
+ * The string must be free()d by the caller.
+ */
+static string dupe_string(string str) {
+  string result = malloc(sizeof(struct string) + str->len);
+  memcpy(result, str, sizeof(struct string) + str->len);
+  return result;
+}
+/* END: String handling */
 
 int main(int argc, char** argv) {
   printf("hello world\n");
